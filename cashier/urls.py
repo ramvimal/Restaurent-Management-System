5d4695aa9f929dc368190065c0341c8173a7c057
Vimal Ram <ramvimal722@gmail.com>
2026-01-09 20:38:05 +0530

    Cashier Section update

--- cashier/urls.py
+++ cashier/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path("login/", views.cashier_login, name="cashier_login"),
     path('dashboard/', views.cashier_dashboard, name='cashier_dashboard'),
     path("logout/", views.cashier_logout, name="cashier_logout"),
-    # path("order/<int:order_id>/", views.cashier_order_detail, name="cashier_order_detail"),
+    path("order/<int:order_id>/", views.cashier_order_detail, name="cashier_order_detail"),
 ]
--- cashier/views.py
+++ cashier/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import Group
-from orders.models import Order
+from orders.models import Order 
+from menu.models import MenuItem
 
 
 def cashier_login(request):
@@ -25,11 +26,31 @@ def cashier_login(request):
 def cashier_dashboard(request):
     if not request.user.groups.filter(name="Cashier").exists():
         return redirect("cashier_login")
-    return render(request,"cashier/dashboard.html")
     
+    panding_orders = Order.objects.filter(status="PENDING").count()
+    total_menu = MenuItem.objects.count()
+    
+    return render(request,"cashier/dashboard.html",
+                {"panding_orders":panding_orders,"total_menu":total_menu}
+                )
 
 def cashier_logout(request):
     logout(request)
     return redirect("cashier_login")
     
+
+def cashier_order_detail(request, order_id):
+    if not request.user.groups.filter(name="Cashier").exists():
+        return redirect("/")
+
+    order = get_object_or_404(Order, id=order_id)
+
+    if request.method == "POST":
+        order.status = "COMPLETED"
+        order.save()
+        return redirect("cashier_dashboard")
+
+    return render(request, "cashier/order_detail.html", {
+        "order": order
+    })
 
